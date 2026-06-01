7168ff6179431813a81cb68ea4a3e9c001b84434
AlexCruzGargallo <feikcruz@gmail.com>
2026-06-01 02:53:53 +0200

    chore: configures github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === 'true' && !!repo;
+
+const site = isGitHubPagesBuild
+  ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io`
+  : 'http://localhost:4321';
+
+const base = isGitHubPagesBuild ? `/${repo}` : '/';
+
 // https://astro.build/config
 export default defineConfig({
+  site,
+  base,
   vite: {
     plugins: [tailwindcss()]
   }
